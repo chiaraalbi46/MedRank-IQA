a7408666ceec704bbffa24beddd8939eb4ffe863
Eleonora Agnoloni <eleonora.agnoloni@edu.unifi.it>
2026-01-28 17:16:33 +0100

    Conflicts solved.

---
+++ provacomit.py
@@ -0,0 +1,9 @@
+
+
+from comet_ml import Experiment 
+
+experiment = Experiment(api_key='Fwcd8Z62iWdyhdkt7y0gYSVQw', project_name='medrank-iqa-finetuning') # Fwcd8Z62iWdyhdkt7y0gYSVQw'
+#experiment = Experiment() ##
+experiment.set_name('prova')
+ek = experiment.get_key()
+
