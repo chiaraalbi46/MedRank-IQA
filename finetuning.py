Fix clamp after loss computation. Fix freeze of backbone when finetuning backbone lr multiplier is set.

finetuning.py
@@ -301,9 +301,9 @@ if __name__ == '__main__':
     # Step 2 fine-tuning 
     model = Step2Model(model)
     model = model.to(device)
-    if args.file_path is not None:
-        for p in model.resnet.parameters():
-            p.requires_grad = False
+    # if args.file_path is not None:
+    #     for p in model.resnet.parameters():
+    #         p.requires_grad = False
 
     trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print(f"Trainable parameters: {trainable_params}")
@@ -316,11 +316,17 @@ if __name__ == '__main__':
             optim_backbone = Adam(model.resnet.parameters(), lr=learning_rate * args.finetune_backbone_lr_multiplier, weight_decay=1e-4)
         else:
             optim_backbone = None
+            if args.file_path is not None:
+                for p in model.resnet.parameters():
+                    p.requires_grad = False
         pretrained_model = args.file_path.split('/')[-1].split('.')[0]
         dest_folder_name = f'finetuning_{pretrained_model}_{train_images}'
     else:
         optim = Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
         optim_backbone = None
+        if args.file_path is not None:
+            for p in model.resnet.parameters():
+                p.requires_grad = False
         
     criterion = nn.MSELoss()
 
@@ -372,7 +378,7 @@ if __name__ == '__main__':
                 if optim_backbone is not None and epoch >= optim_backbone_freeze:
                     optim_backbone.zero_grad()
                 y_pred = model(x)
-                y_pred = torch.clamp(y_pred, 0, 4)  # gt scores are between 0 and 4
+                # y_pred = torch.clamp(y_pred, 0, 4)  # gt scores are between 0 and 4
                 loss = criterion(y_pred, y.unsqueeze(1))
                 t.set_postfix(loss=loss.item())
                 loss.backward()
@@ -409,6 +415,7 @@ if __name__ == '__main__':
                     test_losses.append(loss.item())
 
                     # Salva pred/gt nello stesso ordine
+                    y_pred = torch.clamp(y_pred, 0, 4)
                     all_predictions.append(y_pred.squeeze(1).cpu().numpy())  
                     all_targets.append(y.cpu().numpy())                    
 

finetuningearlystopping.py
@@ -21,7 +21,7 @@ from torch.utils.tensorboard import SummaryWriter
 import torch.nn.functional as F
 from scipy.stats import spearmanr, kendalltau, pearsonr
 
-from pretraining import TARGET_HW, SiameseModel, random_patch_selection
+from pretraining import TARGET_HW, random_patch_selection, SiameseModel
 from utils import build_path_score_dict  # TODO: with the new csv files this function maybe should be avoided...
 
 
@@ -283,7 +283,6 @@ def evaluate_loss(model, dataloader, device, criterion) -> float:
             losses.append(loss.item())
     return float(np.mean(losses)) if len(losses) else float("inf")
 
-
 if __name__ == '__main__':
 
     ###### NB: per ogni test viene creata una cartella con questa struttura a meno che non si passi --dest_folder:
@@ -328,7 +327,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    # device = "cuda:0"
     device = torch.device(f'cuda:{args.device_id}' if torch.cuda.is_available() else 'cpu')
 
     n_epochs = int(args.n_epochs)
@@ -448,9 +446,9 @@ if __name__ == '__main__':
             project_name=args.name_proj
         )
     else:
-        # experiment = Experiment(project_name=args.name_proj)
+        experiment = Experiment(project_name=args.name_proj)
         # if .comet.config is not correctly loaded pass explicitly your COMET_API_KEY
-        experiment = Experiment(project_name=args.name_proj, api_key='Fwcd8Z62iWdyhdkt7y0gYSVQw')
+        # experiment = Experiment(project_name=args.name_proj, api_key='Fwcd8Z62iWdyhdkt7y0gYSVQw')
 
     experiment.set_name(name_exp)  # comet experiment has the same name of the destination folder
     ek = experiment.get_key()
@@ -464,7 +462,7 @@ if __name__ == '__main__':
     early_stopper = None
     if early_stopping_enabled:
         early_stopper = EarlyStopping(
-            patience=4, #10, 20
+            patience=5, #10, 20
             verbose=True,
             delta=1e-4,
             path=best_path,
@@ -542,6 +540,7 @@ if __name__ == '__main__':
                 test_losses.append(loss.item())
 
                 # Salva pred/gt nello stesso ordine
+                y_pred = torch.clamp(y_pred, 0, 4)
                 all_predictions.append(y_pred.squeeze(1).cpu().numpy())
                 all_targets.append(y.cpu().numpy())
 
@@ -600,6 +599,7 @@ if __name__ == '__main__':
             t.set_postfix(loss=loss.item())
             test_losses.append(loss.item())
 
+            y_pred = torch.clamp(y_pred, 0, 4)
             all_predictions.append(y_pred.squeeze(1).cpu().numpy())
             all_targets.append(y.cpu().numpy())
             all_names.extend([os.path.basename(p) for p in paths])