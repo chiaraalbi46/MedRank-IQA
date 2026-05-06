27d52b69f36b79ade5c75ad38fe2b921968991de
chiaraalbi46 <chiaraalbi46@gmail.com>
2026-05-06 16:35:56 +0200

    Remove ignored files from tracking

--- provacomit.py
+++
@@ -1,9 +0,0 @@
-
-
-from comet_ml import Experiment 
-
-experiment = Experiment(api_key='Fwcd8Z62iWdyhdkt7y0gYSVQw', project_name='medrank-iqa-finetuning') # Fwcd8Z62iWdyhdkt7y0gYSVQw'
-#experiment = Experiment() ##
-experiment.set_name('prova')
-ek = experiment.get_key()
-
